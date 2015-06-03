fcd28646d2f26ee136cb7a064032de13862d45f7
Lars Udengaard <lu@version3.dk>
2015-06-03 11:04:13 +0200

    Updated version in AssemblyInfo

--- Cirqus.TypeScript/Properties/AssemblyInfo.cs
+++ Cirqus.TypeScript/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("0.2")]
 [assembly: AssemblyVersion("0.2")]
-[assembly: AssemblyFileVersion("0.2.0")]
+[assembly: AssemblyFileVersion("0.2.1")]
