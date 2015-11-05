463cf102c17d155d4f0fbab0a6a4b461bb89e87d
Asger Hallas <ahl@asgerhallas.dk>
2015-11-05 11:22:08 +0100

    removed the hash from the generated file

--- Cirqus.TypeScript/Generation/ProxyGenerationResult.cs
+++ Cirqus.TypeScript/Generation/ProxyGenerationResult.cs
@@ -18,17 +18,10 @@ namespace Cirqus.TypeScript.Generation
 
         public void WriteTo(string destinationFilePath)
         {
-            if (File.Exists(destinationFilePath) && !HasChanged(destinationFilePath))
-            {
-                Console.WriteLine("    No changes - skipping {0}", destinationFilePath);
-                return;
-            }
-
             Console.WriteLine("    Writing {0}", destinationFilePath);
-            var header = string.Format(HeaderTemplate, HashPrefix, GetHash());
             
             var output = new StringBuilder();
-            output.Append(header);
+            output.Append(HeaderTemplate);
             output.AppendLine("");
             output.AppendLine("");
             output.AppendLine(_code);
@@ -36,45 +29,9 @@ namespace Cirqus.TypeScript.Generation
             File.WriteAllText(destinationFilePath, output.ToString(), Encoding);
         }
 
-        string GetHash()
-        {
-            return Convert.ToBase64String(MD5.Create().ComputeHash(Encoding.GetBytes(_code)));
-        }
-
-        bool HasChanged(string destinationFilePath)
-        {
-            return GetHash() != GetHashFromFile(destinationFilePath);
-        }
-
-        string GetHashFromFile(string destinationFilePath)
-        {
-            using (var file = File.OpenRead(destinationFilePath))
-            using (var reader = new StreamReader(file, Encoding))
-            {
-                string line;
-
-                while ((line = reader.ReadLine()) != null)
-                {
-                    var trimmedLine = line.TrimStart();
-                    
-                    if (string.IsNullOrWhiteSpace(trimmedLine)) break;
-
-                    if (!trimmedLine.StartsWith(HashPrefix)) continue;
-                    
-                    var hash = trimmedLine.Substring(HashPrefix.Length);
-
-                    return hash;
-                }
-            }
-
-            return "";
-        }
-
         const string HeaderTemplate = @"/* 
     Generated with Cirqus.TypeScript.exe
     Should not be edited directly - should probably be regenerated instead :)
-    {0}{1}
 */";
-        const string HashPrefix = "Hash: ";
     }
 }
